46855326af7a1cded19f41b7ffcec29911f38daa
zavodelevator <68054345+zavodelevator@users.noreply.github.com>
2023-10-17 14:39:19 +0300

    Update form_build.rb

--- busket/calcs/form_build.rb
+++ busket/calcs/form_build.rb
@@ -36,45 +36,45 @@
 #     }
 # }
 aa = {
-  'depiction': ['', ''],
+  'hash_name': '',
   'id': ['id', ''],
   'name': ['Назва', ''],
   'description': ['Опис', ''],
-  'kof_markup': ['Коф націнки', ''],
-  'price_$': ['Ціна $', ''],
-  'price_₴': ['Ціна ₴', ''],
-  'personal_data': {
-    'depiction': ['', ''],
-    'area': ['Площа покриття', ''],
-    'metal_thickness': ['Товщина металу  (мм)', ''],
-  },
-  'dimensions': {
-    'depiction': ['', ''],
-    'height': ['Висота', ''],
-    'length': ['Дожина, Вильот', ''],
-    'width': ['Ширина, Габарит', ''],
-    'diam': ['Діаметр', ''],
-    'weight': ['Вага', '']
-  },
   'tags': ['Теги', []],
   'photo': ['Фото', []],
-  'logo': ['Лого', []],
-  'links_drawing': ['Креслення', []],
-  'producer': ['Виробник, Постачальник', []],
-  'shipping_dimensions': {
-    'depiction': ['', ''],
-    'height': ['Висота', ''],
-    'width': ['Ширина', ''],
-    'length': ['Довжина', ''],
-    'packaging_type': ['Тип пакування', '']
-  },
-  'unit': ['Одиниця виміру', ''],
-  'location_warehouse': ['Місце знаходження на складі', ''],
-  'group_crm': ['Группа црм', ''],
-  'sten_on': ['Встановлюється на', ''],
-  'consists_of': ['складається з', ''],
   'other_data': {
-    'depiction': ['', ''],
+    'hash_name': '',
+    'kof_markup': ['Коф націнки', ''],
+    'price_$': ['Ціна $', ''],
+    'price_₴': ['Ціна ₴', ''],
+    'personal_data': {
+      'hash_name': '',
+      'area': ['Площа покриття', ''],
+      'metal_thickness': ['Товщина металу  (мм)', ''],
+    },
+    'dimensions': {
+      'hash_name': '',
+      'height': ['Висота', ''],
+      'length': ['Дожина, Вильот', ''],
+      'width': ['Ширина, Габарит', ''],
+      'diam': ['Діаметр', ''],
+      'weight': ['Вага', '']
+    },
+    'logo': ['Лого', []],
+    'links_drawing': ['Креслення', []],
+    'producer': ['Виробник, Постачальник', []],
+    'shipping_dimensions': {
+      'hash_name': '',
+      'height': ['Висота', ''],
+      'width': ['Ширина', ''],
+      'length': ['Довжина', ''],
+      'packaging_type': ['Тип пакування', '']
+    },
+    'unit': ['Одиниця виміру', ''],
+    'location_warehouse': ['Місце знаходження на складі', ''],
+    'group_crm': ['Группа црм', ''],
+    'sten_on': ['Встановлюється на', ''],
+    'consists_of': ['складається з', '']
   }
 }
 
@@ -84,7 +84,7 @@ end
 
 def aanny(el)
   el.each do |key, value|
-    if value.is_a?(Array) && key != 'depiction' && !value.is_a?(Hash) //problemm
+    if value.is_a?(Array) && key != 'hash_name' && !value.any? { |elem| elem.is_a?(Hash) } && !value.is_a?(Hash) 
       if value[1].is_a?(Array)
         puts "input many elements: #{value[0]}"
       else
