dd1281eebe117d4a52ebaa4d2f4db378051b0ed5
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2023-10-22 22:01:20 +0300

    leyaut and buskket

--- busket/calcs/form_build.rb
+++ busket/calcs/form_build.rb
@@ -1,82 +1,56 @@
-# aa = {
-#     'id': ['id', ''],
-#     'name': ['Назва', ''],
-#     'description': ['Опис', ''],
-#     'kof_markup': ['Коф націнки', ''],
-#     'price_$': ['Ціна', ''],
-#     'personal_data': {
-#         'area': ['Площа покриття', ''],
-#         'metal_thickness': ['Товщина металу  (мм)', ''],
-#     },
-#     'dimensions': {
-#         'height': ['Висота', ''],
-#         'length': ['Дожина, Вильот', ''],
-#         'width': ['Ширина, Габарит', ''],
-#         'diam': ['Діаметр', ''],
-#         'weight': ['Вага', '']
-#     },
-#     'tags': ['Теги', []],
-#     'photo': ['Фото', []],
-#     'logo': ['Лого', []],
-#     'links_drawing': ['Креслення', []],
-#     'producer': ['Виробник, Постачальник', []],
-#     'shipping_dimensions': {
-#         'height': ['Висота', ''],
-#         'width': ['Ширина', ''],
-#         'length': ['Довжина', ''],
-#         'packaging_type': ['Тип пакування', ''],
-#         'unit': ['Одиниця виміру', '']
-#     },
-#     'location_warehouse': ['Місце знаходження на складі', ''],
-#     'group_crm': ['Группа црм', ''],
-#     'sten_on': ['Встановлюється на', ''],
-#     'consists_of': ['складається з', ''],
-#     'other_data': {
+# 'name': ['Назва', ''],
+# 'description': ['Опис', ''],
+# 'id': ['id', ''],
+# 'tags': ['Теги', []],
+# 'group_product': ['Группа продуктов', ''],
+# 'sten_on': ['Встановлюється на', ''],
+# 'consists_of': ['складається з', ''],
+# 'kof_markup': ['Коф націнки', ''],
+# 'price_₴': ['Ціна ₴', ''],
+# 'price_$': ['Ціна $', ''],
 
-#     }
-# }
-aa = {
-  'depiction': ['', ''],
-  'id': ['id', ''],
-  'name': ['Назва', ''],
-  'description': ['Опис', ''],
-  'kof_markup': ['Коф націнки', ''],
-  'price_$': ['Ціна $', ''],
-  'price_₴': ['Ціна ₴', ''],
-  'personal_data': {
-    'depiction': ['', ''],
-    'area': ['Площа покриття', ''],
-    'metal_thickness': ['Товщина металу  (мм)', ''],
-  },
-  'dimensions': {
-    'depiction': ['', ''],
-    'height': ['Висота', ''],
-    'length': ['Дожина, Вильот', ''],
-    'width': ['Ширина, Габарит', ''],
-    'diam': ['Діаметр', ''],
-    'weight': ['Вага', '']
-  },
-  'tags': ['Теги', []],
-  'photo': ['Фото', []],
-  'logo': ['Лого', []],
-  'links_drawing': ['Креслення', []],
-  'producer': ['Виробник, Постачальник', []],
-  'shipping_dimensions': {
-    'depiction': ['', ''],
-    'height': ['Висота', ''],
-    'width': ['Ширина', ''],
-    'length': ['Довжина', ''],
-    'packaging_type': ['Тип пакування', '']
-  },
-  'unit': ['Одиниця виміру', ''],
-  'location_warehouse': ['Місце знаходження на складі', ''],
-  'group_crm': ['Группа црм', ''],
-  'sten_on': ['Встановлюється на', ''],
-  'consists_of': ['складається з', ''],
-  'other_data': {
-    'depiction': ['', ''],
+
+
+  aa = {
+    'name': ['Назва', ''],
+    'description': ['Опис', ''],
+    'id': ['id', ''],
+    'tags': ['Теги', []],
+    'group_product': ['Группа продуктов', ''],
+    'sten_on': ['Встановлюється на', ''],
+    'consists_of': ['складається з', ''],
+    'kof_markup': ['Коф націнки', ''],
+    'price_₴': ['Ціна ₴', ''],
+    'price_$': ['Ціна $', ''],
+    'data': {  
+      'personal_data': {
+        'area': ['Площа покриття', ''],
+        'metal_thickness': ['Товщина металу  (мм)', ''],
+      },
+      'dimensions': {
+        'height': ['Висота', ''],
+        'length': ['Дожина, Вильот', ''],
+        'width': ['Ширина, Габарит', ''],
+        'diam': ['Діаметр', ''],
+        'weight': ['Вага', '']
+      },
+      'logo': ['Лого', []],
+      'links_drawing': ['Креслення', []],
+      'producer': ['Виробник, Постачальник', []],
+      'shipping_dimensions': {
+        'img': ['Картинеки', []],
+        'height': ['Висота', ''],
+        'width': ['Ширина', ''],
+        'length': ['Довжина', ''],
+        'packaging_type': ['Тип пакування', '']
+      },
+      'photo': ['Фото', []],
+      'unit': ['Одиниця виміру', ''],
+      'location_warehouse': ['Місце знаходження на складі', ''],
+      'other_data': {
+      }
+    }  
   }
-}
 
 def form_build(a)
   aanny(a)
@@ -84,7 +58,7 @@ end
 
 def aanny(el)
   el.each do |key, value|
-    if value.is_a?(Array) && key != 'depiction' && !value.is_a?(Hash)
+    if value.is_a?(Array) && key != 'description' && !value.is_a?(Hash)
       if value[1].is_a?(Array)
         puts "input many elements: #{value[0]}"
       else
