9b41828361633b7d40c53938c96e0fd0b1ce0b60
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2023-10-29 10:00:04 +0300

    left_menu

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+//= require l_b_menu
+
+
+
+
---
+++ app/assets/javascripts/l_b_menu.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.getElementById('menu-toggle');
+  const slideOutMenu = document.getElementById('slide-out-menu');
+
+  menuToggle.addEventListener('click', function() {
+      const width = window.innerWidth <= 768 ? '10%' : '20%';
+      if (slideOutMenu.style.width === '0px' || slideOutMenu.style.width === '') {
+          slideOutMenu.style.width = width;
+      } else {
+          slideOutMenu.style.width = '0';
+      }
+  });
+});
